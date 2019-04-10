04149247570858ed8610cee092c1f7cca22a2d01
dlelo <akinyidonattah@gmail.com>
2019-04-10 06:14:03 +0300

    setup appjs

---
+++ index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var mongoose = require('mongoose');
+var pug = require('pug');
+
+app.get('/', function (req, res){
+  response.send('hello');
+});
+
+app.listen(9999);
